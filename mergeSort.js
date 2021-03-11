8e42aac21a32581da64f9f25d0d4838e3d5d4ebc
A77soft <62488561+A77soft@users.noreply.github.com>
2021-03-10 21:03:09 -0500

    Update mergeSort.js
    Updated the notes to be more detailed.

--- mergeSort.js
+++ mergeSort.js
@@ -46,14 +46,35 @@ function mergeSort(array) {
 //A merge sort is a divide and conquer algorithm that splits an unsorted collection into 2 halves
 //It sorts the 2 halves, and then merges them together to form one, sorted collection - by using recursion 
 
+//Merge Sort Theory
+//The basic idea behind merge sort is that it tends to be a lot easier to sort two smaller, sorted lists rather than sorting a single large unsorted list
+//The divide and conquer technique is actually a algorithm design paradigm
+
+//Overview of Divide and Conquer
+//A divide and conquer algorithm divides a problem into simpler versions of itself
+//By breaking down a problem into smaller parts, they become easier to solve - usually, the solution for the smaller sub-problems can be applied to a larger, complicated one
+//Conquering the large problem using the same solution is what makes divide and conquer recursive  
+
+//Steps to Merge Sort
+//1. Divide and break up the problem into the smallest possible "subproblem", of the exact same type.
+//2. Conquer and tackle the smallest subproblems first 
+//Once you've figured out a solution that works, use the exact same technique to solve larger subproblems - in other words, solve the subproblems recursively
+//3. Combine the answers and build up smaller subproblems until you finally end up applying the same solution to the larger, more complicated problem that you started with
+
+//Why it Works Well
+//A list with a single item in it is always going to be considered "sorted"
+//Once we have at least two sorted lists, we can merge them together to create a single, sorted list
+//By splitting up one large, unsorted list down into its individual parts, we can then begin merging those elements together
+//effectively reconstructing our list into a sorted form
+
+//Recursion in Effect
+//The mergeSort function contains a merge function, which actually combines two lists together and sorts them in the correct order 
+//In addition to a mergeSort function, which will continue to split the input array and call the merge function again and again recursively
+//Since merge sort is implemented recursively, we can sort multiple elements/sections of the list at a time
+
 //Basics of Merge Sort
-//1. It contains a merge function, which actually combines two lists together and sorts them in correct order
-//2. There's also a mergeSort function within, which will continue to split the input array and call the merge function again and again
-//3. A list with a single item in it is always going to be considered sorted
-//4. By splitting up one large, unsorted list down into at least two sorted lists, we can merge them together to create one sorted list
-//5. From its individual parts, we can then begin merging those elements together, effectively reconstructing our list into a sorted form
-//6. Merging actually referring to is the act of appending an item in a sorted order to a temporary array structure which builds over time
-//7. If we multiply the log on n by the value of n, the result ends up being the number of total append operations to perform
+//1. Merging actually referring to is the act of appending an item in a sorted order to a temporary array structure which builds over time
+//2. If we multiply the log on n by the value of n, the result ends up being the number of total append operations to perform
 
 //Big O Notation
 //Time Complexity - Linearithmic Runtime O(n Log n), given the amount of time that merge sort needs there's clearly aspects of both at play
