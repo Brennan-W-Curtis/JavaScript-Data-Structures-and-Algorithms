095d945dc5a29859d6d1c609e9e40d79bdb784a9
A77soft <62488561+A77soft@users.noreply.github.com>
2021-03-25 12:19:07 -0400

    Update binarySearchTree.js
    Appended a new implementation method for Breadth First Search.

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -368,7 +368,36 @@ class BST {
         }
         return result;
     }
+    
  }
+
+ function levelOrderSearch(rootNode) {
+    // Check that a root node exists.
+    if (rootNode === null) {
+      return;
+    }
+    // Create our queue and push our root node into it.
+    let queue = [];
+    queue.push(rootNode);
+    // Continue searching through as queue as long as it's not empty.
+    while (queue.length > 0) {
+      // Create a reference to currentNode, at the top of the queue.
+      let currentNode = queue[0];
+      // If currentNode has a left child node, add it to the queue.
+      if (currentNode.left !== null) {
+        queue.push(currentNode.left)
+      }
+      // If currentNode has a right child node, add it to the queue.
+      if (currentNode.right !== null) {
+        queue.push(currentNode.right)
+      }
+      // Remove the currentNode from the queue.
+      queue.shift()
+    }
+    // Continue looping through the queue until it's empty!
+  }
+
+  //Time Complexity - O(n), Linear Runtime
 
  const bts = new BST();
 
